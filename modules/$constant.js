babf2dc5c3c37721c0a0749ca18668ebabacea6a
Mark Hahn <mark@hahnca.com>
2016-03-06 20:11:16 -0800

    cleanup

--- modules/$constant.js
+++ modules/$constant.js
@@ -1,8 +1,8 @@
 
 let data = this.get('$data');
 if(this.isPin('$in'))
-  this.react('$in', (_, valIn) => {
-    this.emit('$out', data, valIn.meta);
+  this.react('$in', (_, __, meta) => {
+    this.emit('$out', data, meta);
   });
 else
   this.put('$out', data);
--- modules/$http.js
+++ modules/$http.js
@@ -1,20 +1,22 @@
 
 const http = require('http');
-let resByReqWeak = new WeakMap();
+let resByReqId = {};
+let reqId = 0;
 
 switch(this.get('$op')) {
 
   case 'server':
-    const hostname = this.get('host') || '127.0.0.1';
-    const port = this.get('port') || 8080;
+    const hostname = this.get('$host') || '127.0.0.1';
+    const port = this.get('$port') || 8080;
     http.createServer((req, res) => {
-      resByReqWeak.set(req, res);
-      this.emit('$req', req);
+      resByReqId[++reqId] = res;
+      this.emit('$req', null, 
+          {reqId, reqUrl: req.url, reqHdrs: req.headers, reqMethod: req.method});
     }).listen(port, hostname, _=> {
       this.log(`Server running at http://${hostname}:${port}/`);
     });
     this.react('$res', (_, data, meta) => {
-      let res = resByReqWeak.get(meta.req);
+      let res = resByReqId[meta.reqId];
       res.writeHead(meta.resCode || 200, meta.hdrs || {'Content-Type': 'text/plain' });
       res.end(data);
     });
